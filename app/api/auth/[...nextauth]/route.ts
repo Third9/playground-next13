e8ea807004e0fcaa2ba0774c5ef5bffe57aa9335
Hyeock Kwon <dev.third9@gmail.com>
2023-09-18 23:23:27 +0900

    feat: next-auth를 이용한 naver 인증 기본 구현

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from 'next-auth/providers/naver'
 
 const handler = NextAuth({
     providers: [
@@ -7,6 +8,10 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_OAUTH_ID || "",
             clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
         }),
+        NaverProvider({
+            clientId: process.env.NAVER_OAUTH_ID || "",
+            clientSecret: process.env.NAVER_OAUTH_SECRET || "",
+        })
     ],
 })
 
