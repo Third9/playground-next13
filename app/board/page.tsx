4c897096e9ffee714a1a8fd36df1dc18dd023fc9
Hyeock Kwon <dev.third9@gmail.com>
2023-10-08 14:48:05 +0900

    feat: 간단한 내부 api 선언

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,7 +1,25 @@
+'use client'
+
 export default function Board() {
+
+    const getHello = async () => {
+        try {
+            const res = await fetch('/api/boards', {
+                method: 'GET',
+            })
+            const data = await res.json()
+            console.log('res >> ', data)
+        } catch (error) {
+         console.log('error >> ', error)
+        }
+    }
+
+
     return (
         <div>
-            Board
+            <div>Board</div>
+
+            <button onClick={getHello}>click</button>
         </div>
     )
 }
